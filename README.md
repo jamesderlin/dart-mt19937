6a3895ce163cf7a8c0bdf4b18aeb88fa5ab31149
James Lin <jamesdlin@gmail.com>
2022-04-22 13:06:01 -0700

    Add a `Random` implementation for MT19937-64
    Add a `Random` implementation for MT19937-64 so that people who use
    MT19937-64 don't have to use such a significantly different API.

--- README.md
+++ README.md
@@ -13,47 +13,29 @@ import 'package:fixnum/fixnum.dart';
 import 'package:mt19937/mt19937.dart';
 
 void main() {
-  // Provides a raw generator.
+  // Provides a raw MT19937 generator.
   var mt = MersenneTwister();
   print(List<int>.generate(10, (_) => mt()));
 
-  // Also provides a [Random] interface.
+  // Also provides [Random] interfaces.
   var random = RandomMt19937();
   print(List<int>.generate(10, (_) => random.nextInt(100)));
   print(List<double>.generate(10, (_) => random.nextDouble()));
 
-  // MT19937-64 is implemented with a slightly different interface.
+  var random64 = RandomMt19937_64();
+  print(
+    List<BigInt>.generate(
+      10,
+      (_) => random64.nextBigInt(random64.exclusiveMax),
+    ),
+  );
+
+  // Also provides a configurable engine that is preconfigured for
+  // MT19937 (32-bit words) and MT19937-64 (64-bit words).
   var seed = DateTime.now().millisecondsSinceEpoch;
   var mt64 = MersenneTwisterEngine.w64()..init(Int64(seed));
   print(List<BigInt>.generate(10, (_) => mt64()));
 }
 ```
 
-## A note on MT19937-64
-
-Why is the MT19937-64 implementation different?  The MT19937-64 implementation
-is provided as part of a *general* [`MersenneTwisterEngine`] implementation.
-From a different perspective, an equivalent question is: why is the 32-bit
-MT19937 implementation different?
-
-This package provides a specialized implementation for MT19937 because
-* It's able to provide a portable `int`-based interface, which is more
-  convenient to use.
-* It's able to provide an `int`-based *implementation* for Dart VM platforms,
-  which is *significantly* faster.
-
-In contrast, those factors would not apply to a specialized MT19937-64
-implementation:
-* Dart does not have a 64-bit unsigned integer type, whether as a built-in type
-  from `dart:core` or as a type from [`package:fixnum`].  To return non-negative
-  values that use the full 64-bit range, the MT19937-64 implementation therefore
-  must return `BigInt`s.
-* Returning `BigInt`s negates all performance benefits from an `int`-based
-  implementation.
-
-Providing and maintaining specialized `int` (for Dart VM platforms) and `Int64`
-(for Dart for the Web) implementations of MT19937-64 therefore does not seem to
-be worthwhile.
-
 [Mersenne Twister 19937]: https://en.wikipedia.org/wiki/Mersenne_twister
-[`package:fixnum`]: https://pub.dev/packages/fixnum
--- lib/src/random.dart
+++ lib/src/random.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:fixnum/fixnum.dart';
+
 import '../mt19937.dart';
 
 /// An implementation of [Random] that is backed by a lower-level random number
@@ -7,7 +9,7 @@ import '../mt19937.dart';
 class WrappedRandom implements Random {
   /// The raw random number generator.
   ///
-  /// Expected to return a number in the range \[0, exclusiveMax).
+  /// Expected to return a number in the range \[0, [exclusiveMax]).
   final int Function() generator;
 
   /// The exclusive maximum value returnable by [generator].
@@ -49,19 +51,19 @@ class WrappedRandom implements Random {
 
 /// An implementation of [Random] that is backed by a lower-level random number
 /// generator that returns [BigInt]s.
-class WrappedRandomBigInt implements Random {
+class WrappedBigRandom implements Random {
   /// The raw random number generator.
   ///
-  /// Expected to return a number in the range \[0, exclusiveMax).
+  /// Expected to return a number in the range \[0, [exclusiveMax]).
   final BigInt Function() generator;
 
   /// The exclusive maximum value returnable by [generator].
   final BigInt exclusiveMax;
 
   /// Constructor.
-  WrappedRandomBigInt(this.generator, this.exclusiveMax);
+  WrappedBigRandom(this.generator, this.exclusiveMax);
 
-  /// TOOD
+  /// Returns the next [BigInt] in the range \[0, [max]).
   BigInt nextBigInt(BigInt max) {
     if (!(max > BigInt.zero && max <= exclusiveMax)) {
       throw RangeError(
@@ -92,13 +94,47 @@ class WrappedRandomBigInt implements Random {
   double nextDouble() => nextBigInt(exclusiveMax) / exclusiveMax;
 
   @override
-  bool nextBool() => nextBigInt(BigInt.two) == BigInt.zero;
+  bool nextBool() => nextInt(2) == 0;
 }
 
 /// A [Random] implementation that uses [MersenneTwister] (MT19937) as a
 /// source for pseudo-random numbers.
 class RandomMt19937 extends WrappedRandom {
   /// Constructs a [RandomMt19937] with an optional seed.
+  ///
+  /// Note that if no seed is provided, a fixed default seed will be used.
   RandomMt19937({int? seed})
       : super(MersenneTwister(seed: seed).call, MersenneTwister.max + 1);
+}
+
+/// A [Random] implementation that uses [MersenneTwister] (MT19937) as a
+/// source for pseudo-random numbers.
+// ignore: camel_case_types
+class RandomMt19937_64 extends WrappedBigRandom {
+  /// Constructs a [RandomMt19937_64] with an optional seed.
+  ///
+  /// [seed] must be an [int], [Int64], or [BigInt].
+  ///
+  /// Note that if no seed is provided, a fixed default seed will be used.
+  factory RandomMt19937_64({Object? seed}) {
+    var mt = MersenneTwisterEngine.w64();
+    if (seed != null) {
+      Int64 normalizedSeed;
+      if (seed is int) {
+        normalizedSeed = Int64(seed);
+      } else if (seed is Int64) {
+        normalizedSeed = seed;
+      } else if (seed is BigInt) {
+        normalizedSeed = Int64.parseInt(seed.toString());
+      } else {
+        throw ArgumentError('Invalid seed type: ${seed.runtimeType}');
+      }
+      mt.init(normalizedSeed);
+    }
+
+    return RandomMt19937_64._(mt.call, mt.max + BigInt.one);
+  }
+
+  RandomMt19937_64._(BigInt Function() generator, BigInt exclusiveMax)
+      : super(generator, exclusiveMax);
 }
